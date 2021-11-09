c956a2409282374ba90475d93271bcc7ca35d1e7
saifulislammunna <saifulmunna28@gmail.com>
2021-11-10 01:44:19 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+ 
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound/NotFound';
+import Login from './Pages/Login/Login/Login';
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+       <Switch>
+         {/*  <PrivateRoute path="/appointment">
+            <Appointment/>
+          </PrivateRoute > */}
+          <Route path="/home">
+            <Home/>
+          </Route>
+           <Route path="/login">
+            <Login/>
+          </Route>  
+         {/*  <Route path="/Register">
+            <Register/>
+          </Route> */}
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="*">
+                     <NotFound></NotFound>
+          </Route>
+        </Switch>
+       </Router>
     </div>
   );
 }

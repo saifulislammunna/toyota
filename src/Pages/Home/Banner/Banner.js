4881b874e0504cbb844b5c322c4d4e2fdc4ef7e5
saifulislammunna <saifulmunna28@gmail.com>
2021-11-12 18:31:17 +0600

    banner-added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { init } from 'ityped';
+
 import './Banner.css';
 const Banner = () => {
+
+    const textRef = useRef();
+    useEffect(() => {
+
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 70,
+             strings: ['  Toyota .']
+        })
+    })
     return (
         <div>
             
              
              <div>
               <div className="carousel-part background-image">
-              <div className="left-container  half-width p-5">
+              <div className="left-container  half-width ">
                      <div className="text-container">
-                         <h1 className="fs-1 fw-bold banner-text  " >Your Cars, <br />
-                        Best Choose.</h1>
-                         {/* <h3 className="text-animation fs-1 banner-text"> <span  ref={textRef}></span></h3>
- */}
+                         <h1 className="fs-1 fw-bold text-white " >Once You Drive One, <br />
+                       You'll never stop.</h1>
+                         <h3 className="text-animation fs-1 text-white"> <span  ref={textRef}></span></h3>
+  
                            <Link  to="/products"><Button>Explore</Button></Link> 
                      </div>
                 </div>

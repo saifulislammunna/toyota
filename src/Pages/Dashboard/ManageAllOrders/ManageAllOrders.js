09d8187fc56f209ab08b2523db09489c14c45d7c
saifulislammunna <saifulmunna28@gmail.com>
2021-11-12 11:28:31 +0600

    manage-allorders -&-products-added

--- src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button , Table } from 'react-bootstrap';
+ 
 
 const ManageAllOrders = () => {
+  
+    const [orders, setOrders] = useState([]);
+   
+
+    useEffect(()=>{
+        const url = `https://fierce-sands-31991.herokuapp.com/orders`
+        /* console.log(url); */
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>  setOrders(data));
+    },[]);
+
+    const handleDelete = _id => {
+      const url =  `https://fierce-sands-31991.herokuapp.com/orders/${_id}`
+      fetch(url,{
+          method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data); 
+         if(data.deletedCount){
+              alert('Are you sure')
+              const remaining = orders.filter(order => order._id !== _id);
+              setOrders(remaining);
+
+        } 
+       
+      })
+  };
+     
     return (
         <div>
-            <h1>This is manage All orders</h1>
+            <h2>Manage All   Orders : {orders.length}</h2>
+            <Table striped bordered hover>
+  <thead>
+    <tr>
+       
+      <th>Name</th>
+      <th>Email Address</th>
+      <th>Product Id</th>
+      <th>Deleting Order</th>
+    </tr>
+  </thead>
+  <tbody>
+      {orders.map((order)=> (  
+        
+      <tr key={order._id}>
+      
+      <td>{order.name}</td>
+      <td>{order.email}</td>
+      <td>{order._id}</td>
+      <td><Button  onClick={ () => handleDelete(order._id)} className="bg-danger">Delete</Button ></td>
+    </tr>))}
+   
+   
+    
+     
+  </tbody>
+</Table>
         </div>
     );
 };
--- src/Pages/Dashboard/ManageProducts/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts/ManageProducts.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button , Table } from 'react-bootstrap';
+ 
 
 const ManageProducts = () => {
+  
+    const [products, setProducts] = useState([]);
+   
+
+    useEffect(()=>{
+        const url = `https://fierce-sands-31991.herokuapp.com/products`
+        /* console.log(url); */
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>  setProducts(data));
+    },[]);
+
+    const handleDelete = _id => {
+      const url =  `https://fierce-sands-31991.herokuapp.com/products/${_id}`
+      fetch(url,{
+          method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data); 
+         if(data.deletedCount){
+              alert('Are you sure')
+              const remaining = products.filter(product => product._id !== _id);
+              setProducts(remaining);
+
+        } 
+       
+      })
+  };
+     
     return (
         <div>
-            <h1>This is Manage Products</h1>
+            <h2>Manage All   Products : {products.length}</h2>
+            <Table striped bordered hover>
+  <thead>
+    <tr>
+       
+      <th>Name</th>
+      <th>Image</th>
+      <th>Product Id</th>
+      <th>Deleting Products</th>
+    </tr>
+  </thead>
+  <tbody>
+      {products.map((product)=> (  
+        
+      <tr key={product._id}>
+      
+      <td>{product.name}</td>
+      <td><img src={product.img} alt="" /> </td>
+      <td>{product._id}</td>
+      <td><Button  onClick={ () => handleDelete(product._id)} className="bg-danger">Delete</Button ></td>
+    </tr>))}
+   
+   
+    
+     
+  </tbody>
+</Table>
         </div>
     );
 };

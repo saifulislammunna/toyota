77a65a0134e39bb0a06be38e1b8f6f25f880b028
saifulislammunna <saifulmunna28@gmail.com>
2021-11-13 15:44:07 +0600

    style-added

--- src/Pages/Shared/Connect/Connect.js
+++ src/Pages/Shared/Connect/Connect.js
@@ -3,14 +3,14 @@ import { Button } from 'react-bootstrap';
  
 import './Connect.css';
 const Connect = () => {
-  
+
     return (
         <div className= "p-5  touch">
               
        <div    className="p-5 d-flex justify-content-center align-items-center rounded-3">
       <div>
-        <h1>Let's Stay In Touch</h1>
-        <p className="p-2">Get updates on sales, specials and more</p>
+        <h1>Let's Stay In Contact</h1>
+        <p className="p-2">Get updates on news, specials and more</p>
         <input  className="form-control" type="text" placeholder="Your Email"/>
         <br/>
         <Button type="submit" >Submit</Button >
